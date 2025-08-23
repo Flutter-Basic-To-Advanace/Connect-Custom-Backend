4b0658be96e765631e1a07a77b405226e6de04f5
sharada417 <sharada.kanchana2001@gmail.com>
2025-08-23 15:25:34 +0530

    implment user router

--- Backend_Node/Routes/user.js
+++ Backend_Node/Routes/user.js
@@ -1,2 +1,8 @@
 const express  = require('express')
 const router = express.Router();
+
+const userController = require("../controllers/user")
+
+router.post("/users",userController.createUser)
+
+module.exports = router;
